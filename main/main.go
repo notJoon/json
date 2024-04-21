be7cf6533bdd597add688e5e1682173d7070b438
Lee ByeongJun <lbj199874@gmail.com>
2024-04-21 13:28:32 +0900

    revert

---
+++ main/main.go
@@ -0,0 +1,29 @@
+package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/notJoon/json"
+// )
+
+func main() {
+	// path := "$.store.book[0].title"
+
+	// rootNode := json.ObjectNode("", map[string]*json.Node{
+	// 	"store": json.ObjectNode("store", map[string]*json.Node{
+	// 		"book": json.ArrayNode("book", []*json.Node{
+	// 			json.ObjectNode("0", map[string]*json.Node{
+	// 				"title": json.StringNode("title", "Book Title"),
+	// 			}),
+	// 		}),
+	// 	}),
+	// })
+
+	// res := json.Evaluate(rootNode, path)
+	// if res == nil {
+	// 	fmt.Println("Not found")
+	// 	return
+	// } else {
+	// 	fmt.Println(res)
+	// }
+}
